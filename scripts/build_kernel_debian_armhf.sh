7273fc1f8931da0b9a3502da4ff6491c5b65b960
Antony Rheneus <arheneus@marvell.com>
2019-11-08 17:09:22 +0530

    updated sysctl and fw_env
    Signed-off-by: Antony Rheneus <arheneus@marvell.com>

--- scripts/build_kernel_debian_armhf.sh
+++ scripts/build_kernel_debian_armhf.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 declare -a FILES
 declare -a MODULES
@@ -49,7 +49,6 @@ build_deb()
     do
         echo "Adding kernel $f"
         mkdir -p ${TMP}/etc/modules-load.d/
-        echo "$f" >> ${TMP}/etc/modules-load.d/marvell.conf
     done
     i=0
     for f in ${MISC_FILES[*]}
@@ -68,7 +67,19 @@ build_deb()
     done
 
     # i2c-dev is missing in modules
+    mkdir -p ${TMP}/etc/modules-load.d/
     echo "i2c-dev" >> ${TMP}/etc/modules-load.d/marvell.conf
+    echo "$f" >> ${TMP}/etc/modules-load.d/marvell.conf
+    mkdir -p ${TMP}/etc/sysctl.d/
+    echo "sysctl -w net.ipv4.neigh.default.gc_thresh1=16000"    >> ${TMP}/etc/sysctl.d/98-sysctl.conf
+    echo "sysctl -w net.ipv4.neigh.default.gc_thresh2=32000"    >> ${TMP}/etc/sysctl.d/98-sysctl.conf
+    echo "sysctl -w net.ipv4.neigh.default.gc_thresh3=48000"    >> ${TMP}/etc/sysctl.d/98-sysctl.conf
+    echo "sysctl -w net.ipv6.neigh.default.gc_thresh1=8000 "    >> ${TMP}/etc/sysctl.d/98-sysctl.conf
+    echo "sysctl -w net.ipv6.neigh.default.gc_thresh2=16000"    >> ${TMP}/etc/sysctl.d/98-sysctl.conf
+    echo "sysctl -w net.ipv6.neigh.default.gc_thresh3=32000"    >> ${TMP}/etc/sysctl.d/98-sysctl.conf
+    FW_ENV='/dev/mtd0 \t\t 0x00500000 \t 0x80000 \t 0x100000 \t 8'
+    echo -e $FW_ENV > ${TMP}/etc/fw_env.config
+
 
     echo "Package: ${PKG_NAME}-${VERSION}-${ARCH} " > ${TMP}/DEBIAN/control
     echo "Version: $VERSION" >> ${TMP}/DEBIAN/control
