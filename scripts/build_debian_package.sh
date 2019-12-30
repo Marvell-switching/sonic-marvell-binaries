13e6f3d5ca624836b9c11ccce636cf8e09284906
Antony Rheneus <arheneus@marvell.com>
2019-12-29 23:20:59 -0800

    updated eeprom
    Signed-off-by: Antony Rheneus <arheneus@marvell.com>

---
+++ scripts/build_debian_package.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+
+ARCH=arm64
+VERSION=4.9.168
+PKG_NAME=linux-image
+TMP=${PKG_NAME}-${VERSION}-${ARCH}
+DATE=`date -u`
+
+build_deb()
+{
+    rm -fr ${TMP}
+    mkdir -p ${TMP}/DEBIAN
+    mkdir -p ${TMP}/boot/
+    mkdir -p ${TMP}/lib/modules/${VERSION}/
+
+        echo "Adding files from x/"
+        cp -drv x/* ${TMP}/
+
+    echo "Package: ${PKG_NAME}-${VERSION}-${ARCH} " > ${TMP}/DEBIAN/control
+    echo "Version: $VERSION" >> ${TMP}/DEBIAN/control
+    echo "Section: base" >> ${TMP}/DEBIAN/control
+    echo "Priority: optional" >> ${TMP}/DEBIAN/control
+    echo "Architecture: $ARCH" >> ${TMP}/DEBIAN/control
+    echo "Depends:" >> ${TMP}/DEBIAN/control
+    echo "Maintainer: $USER <$USER@marvell.com>" >> ${TMP}/DEBIAN/control
+    echo "Date: $DATE" >> ${TMP}/DEBIAN/control
+    echo "Description: Linux Marvell Kernel" >> ${TMP}/DEBIAN/control
+    #cat ${TMP}/DEBIAN/control
+    echo "Building deb package"
+    dpkg-deb --build ${TMP}
+
+    dpkg -I ${TMP}.deb
+
+}
+
+
+build_deb
